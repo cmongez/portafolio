c7a8af18b13b2dc1e54fcb02562611317b2682a5
Cesar <camongez28@gmail.com>
2022-06-20 04:00:08 -0400

    particles stric mode error  fixed

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,60 @@
 <template>
   <div id="app">
     
-    <router-view/>
+    <Particles />
+    <router-view />
   </div>
 </template>
-
+<script>
+import Particles from "@/components/Particles.vue";
+export default {
+  name: "App",
+  components: {
+    Particles,
+  },
+};
+</script>
 <style lang="scss">
-*{
+* {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  font-family: 'Poppins', sans-serif;
+  font-family: "Poppins", sans-serif;
 }
-$prueba:blue;
-.p-indent{
+$prueba: blue;
+.p-indent {
   text-indent: 30px;
-
-
 }
-a{
+a {
   cursor: pointer;
 }
-body{
-  background-color: #0C151D;
+body {
+  background-color: #0c151d;
 }
-h1{
-  color: #F1F2F4;
+h1 {
+  color: #f1f2f4;
 }
-h2{
-  color: #F1F2F4;
+h2 {
+  color: #f1f2f4;
 }
-p,li{
-  color:#c2c6c9;
+p,
+li {
+  color: #c2c6c9;
 }
-.btn{
-  
+.btn {
   font-weight: 400;
 }
-.btn-v-primary{
+.btn-v-primary {
   background-color: #ffc300;
-  color:#2e2e2e;
-  border:0;
+  color: #2e2e2e;
+  border: 0;
 }
-.btn-v-secondary{
-  background: #171F26;
-  color: #A3ABB2;
+.btn-v-secondary {
+  background: #171f26;
+  color: #a3abb2;
 }
-.card-text,.list-group-item{
-  color: #3D3D3D;
+.card-text,
+.list-group-item {
+  color: #3d3d3d;
 }
-
 </style>
