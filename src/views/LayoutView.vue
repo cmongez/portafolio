07ba884bf152a8507c47fc6fda21c5e9c767daed
Cesar <camongez28@gmail.com>
2022-06-20 03:00:38 -0400

    vue-particles

--- src/views/LayoutView.vue
+++ src/views/LayoutView.vue
@@ -9,13 +9,11 @@
 <script>
 import NavBar from '@/components/NavBar.vue'
 import Footer from '@/components/Footer.vue'
-import Particles from '@/components/Particles.vue';
 export default {
 name:'LayoutView',
 components:{
   NavBar,
   Footer,
-  Particles
 }
 }
 </script>
