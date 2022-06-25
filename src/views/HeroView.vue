ca218e5946ab263eae6d98c651d0dfb74a1c426a
Cesar <camongez28@gmail.com>
2022-06-25 18:47:26 -0400

    styles hero

--- src/views/HeroView.vue
+++ src/views/HeroView.vue
@@ -1,20 +1,21 @@
 <template>
-  <header class="container-fluid">
+  <header class="container-fluid d-flex justify-content-center  align-items-center">
     <div
       class="
         row
         hero
         flex-column
         justify-content-around
+        
         align-items-center
         mt-3
         p-1
         mt-md-4
       "
     >
-      <div class="d-flex flex-column col-sm-6 col-sm-10 col-md-8  col-lg-6 justify-content-around" >
+      
         <img
-          class="align-self-center border-profile img-fluid hero__img col-4 col-md-4 mb-4 mb-md-0 p-0"
+          class="align-self-center border-profile img-fluid hero__img col-4 col-md-4 mb-3 mb-md-0 p-0"
           src="../assets/profile-picture.png"
           alt="César Mongez foto"
           title="Foto de perfil"
@@ -24,19 +25,17 @@
         <h2 class="hero__subtitle h5 mb-4 text-center ">
           Desarrollador Front End
         </h2>
-        <SocialNetworks class="w-50" />
+        
       </div>
-      </div>
-      <div
+      <SocialNetworks />
+        <div
         class="
           hero__buttonGroup
-          col-sm-6 col-sm-9 col-md-7 col-lg-5 
           d-flex
           justify-content-center
-          justify-content-sm-around
           align-items-center
           flex-wrap
-          mt-3 mt-sm-4
+          
         "
       >
         <a  href="001_Cesar_Mongez_Front_End_Curriculum_Vitae.pdf" target="_blank">
@@ -53,6 +52,8 @@
           Contáctame
         </button></a>
       </div>
+   
+      
     </div>
   </header>
 </template>
@@ -68,14 +69,18 @@ export default {
 </script>
 
 <style lang="scss" scoped >
-.hero {
+header{
   min-height: 92vh;
+}
+.hero {
+  height: 500px;
   &__img {
     height: auto;
     max-width: 215px;
     border-radius: 100%;
     background-color: #666;
   }
+ 
 }
 .border-profile {
   border: 10px solid #ffc300;
