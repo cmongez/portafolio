f4a23d48469279243baaa5cce352e5dc96fb9ba5
Cesar <camongez28@gmail.com>
2023-01-20 23:55:45 -0300

    new certificate added

--- src/views/CertificatesView.vue
+++ src/views/CertificatesView.vue
@@ -1,45 +1,37 @@
 <template>
-  <section id="certificados"
-   class=" col-12 col-sm-10 col-md-9 mb-4 container-fluid  certificates" >
+  <section id="certificados" class="col-12 col-sm-10 col-md-9 mb-4 container-fluid certificates">
     <h2>Formación Adicional</h2>
-    <ul class=" pl-4 certificates__list">
+    <ul class="pl-4 certificates__list">
       <li class="certificates__item">
-       
-          Desarrollador Aplicaciones Front End (Bootcamp) <span class="certificates__link"> [En espera del
-          certificado]</span>
+        Desarrollador Aplicaciones Front End (Bootcamp)
+        <a href="certificates/CertificadoBootcampTalentoDigitalCesarMongez.pdf" target="_blank"
+          ><i class="fa-solid fa-up-right-from-square certificates__link" aria-hidden="true"></i
+        ></a>
       </li>
       <li class="certificates__item">
-        
-          Scrum Fundamentals Certified <a
-          href="certificates/CertificadoSCRUMFundamentalsCesarMongez.pdf"
-          target="_blank"
-        ><i class="fa-solid fa-up-right-from-square certificates__link" aria-hidden="true"></i></a
-        >
+        Scrum Fundamentals Certified
+        <a href="certificates/CertificadoSCRUMFundamentalsCesarMongez.pdf" target="_blank"
+          ><i class="fa-solid fa-up-right-from-square certificates__link" aria-hidden="true"></i
+        ></a>
       </li>
-      
+
       <li class="certificates__item">
-        
-          Fundamentos del desarrollo web: Full Stack o Front-end (LinkedIn
-          Learning) <a
-          href="certificates/CertificadoFundamentosdeldesarrolloFrontendCesarMongez.pdf"
-          target="_blank"
-        ><i class="fa-solid fa-up-right-from-square certificates__link" aria-hidden="true"></i></a
-        >
+        Fundamentos del desarrollo web: Full Stack o Front-end (LinkedIn Learning)
+        <a href="certificates/CertificadoFundamentosdeldesarrolloFrontendCesarMongez.pdf" target="_blank"
+          ><i class="fa-solid fa-up-right-from-square certificates__link" aria-hidden="true"></i
+        ></a>
       </li>
       <li class="certificates__item">
-        
-          Introducción al Desarrollo Web: HTML y CSS I y II (Google Actívate) <a
-          href="certificates/CertificadoHTMLCSSIyIIGoogleActivateCesarMongez.pdf"
-          target="_blank"
-        ><i class="fa-solid fa-up-right-from-square certificates__link " aria-hidden="true"></i></a
-        >
+        Introducción al Desarrollo Web: HTML y CSS I y II (Google Actívate)
+        <a href="certificates/CertificadoHTMLCSSIyIIGoogleActivateCesarMongez.pdf" target="_blank"
+          ><i class="fa-solid fa-up-right-from-square certificates__link" aria-hidden="true"></i
+        ></a>
       </li>
       <li class="certificates__item">
-        Comercio Electrónico (Google Actívate) <a
-          href="certificates/CertificadoComercioElectronicoGoogleActivate.pdf"
-          target="_blank"
-          ><i class="fa-solid fa-up-right-from-square certificates__link" aria-hidden="true"></i></a
-        >
+        Comercio Electrónico (Google Actívate)
+        <a href="certificates/CertificadoComercioElectronicoGoogleActivate.pdf" target="_blank"
+          ><i class="fa-solid fa-up-right-from-square certificates__link" aria-hidden="true"></i
+        ></a>
       </li>
     </ul>
   </section>
@@ -47,17 +39,14 @@
 
 <script>
 export default {
-  name: "CertificatesView",
+  name: 'CertificatesView',
 };
 </script>
 
 <style lang="scss" scoped>
-.certificates{
-  &__link{
-    color:#ffc300;
+.certificates {
+  &__link {
+    color: #ffc300;
   }
 }
-
-
-
 </style>
